6a6ebfb46a08048414b7eb1665f2f36aee581c98
vital-fernandez <vital.fernandez@gmail.com>
2022-01-20 09:58:36 -0300

    LiMe 0.2.8 Documentation example 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.abspath(os.pardir)))
-autodoc_mock_imports = ["lmfit", 'scipy']
+autodoc_mock_imports = ['lmfit', 'scipy', 'specutils']
 
 # -- Project information -----------------------------------------------------
 
