dc6a9e43db4c5c752ac6bf0beca16f5bd0a26d37
Vital Fernández <vital.fernandez@userena.cl>
2022-07-17 00:21:11 -0400

    LiMe 0.8.7 Machine learning line detection model_v2, logger, mplcursor pop uses units

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2021, Vital-Fernandez'
 author = 'Vital-Fernandez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.6'
+release = '0.8.7'
 
 
 # -- General configuration ---------------------------------------------------
