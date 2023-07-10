03718f12eb3ed8b7990bb6d810ba0e5c6d7f9960
Vital-Fernandez <vital.fernandez@gmail.com>
2023-07-10 16:31:06 +0200

    LiMe 0.9.59 Corrections for numpy non-masked operations

--- tests/test_line.py
+++ tests/test_line.py
@@ -1,16 +1,16 @@
-# from lime.transitions import Line, label_decomposition
-#
-#
-# def test_line_components():
-#
-#     line = 'O3_5007A'
-#     particle, wavelength, latex = label_decomposition(line)
-#
-#     assert particle[0] == 'O3'
-#
-#     assert wavelength[0] == 5007.0
-#
-#     assert latex[0] == r'$[OIII]5007\AA$'
-#
-#     return
+from lime.transitions import Line, label_decomposition
+
+
+def test_line_components():
+
+    line = 'O3_5007A'
+    particle, wavelength, latex = label_decomposition(line)
+
+    assert particle[0] == 'O3'
+
+    assert wavelength[0] == 5007.0
+
+    assert latex[0] == r'$[OIII]5007\AA$'
+
+    return
 
