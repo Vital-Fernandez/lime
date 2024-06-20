af25741cab298e199e7d8369db13ceb132a89004
Vital <vgf@umich.edu>
2024-06-20 18:18:23 -0400

    Work 20/06/2024

--- examples/tutorials/various/megara_example.py
+++ examples/tutorials/various/megara_example.py
@@ -11,12 +11,12 @@ ngc5471 = lime.Cube.from_file(megara_cube_address, instrument='megara', redshift
 ngc5471.unit_conversion(units_flux='FLAM', norm_flux=1e-18)
 
 # Interactive plot (right click change spaxel)
-# ngc5471.check.cube('He1_5876A')
+ngc5471.check.cube('He1_5876A')
 
 # Generate a spatial mask
 mask_file = './He1_5876A_3level_mask.fits'
-# ngc5471.spatial_masking('He1_5876A', param='SN_line', contour_pctls=[56, 80, 96], output_address=mask_file)
-# ngc5471.plot.cube('He1_5876A', masks_file=mask_file)
+ngc5471.spatial_masking('He1_5876A', param='SN_line', contour_pctls=[56, 80, 96], output_address=mask_file)
+ngc5471.plot.cube('He1_5876A', masks_file=mask_file)
 
 # Adjust mask manually (middle button add or remove spaxels from mask)
 ngc5471.check.cube('He1_5876A', masks_file=mask_file)
